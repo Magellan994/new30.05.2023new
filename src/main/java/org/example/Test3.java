9d6b45ad82bcf7a5aa581a9f0afd453e0c6eb29a
Magellan994 <khukha994@gmail.com>
2023-05-30 22:46:20 +0300

    Initial commit

---
+++ src/main/java/org/example/Test3.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        int Alex = 7;
+        int Maks = 9;
+        Maks++;
+        System.out.println(Alex);
+        System.out.println(Maks);
+
+    }
+}
---
+++ src/main/java/org/example/third.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class third {
+    public static void main(String[] args) {
+
+        System.out.println("c:\\Program Files\\Jav\\jdk1.7.0\\bin\\");
+
+    }
+}
